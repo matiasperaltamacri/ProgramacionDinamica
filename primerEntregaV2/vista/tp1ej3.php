7b7f2b608c28a44a70b47d5772b74234dee5c682
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-08 17:16:15 -0300

    Revert "tp1ej3 con bootstrap y re-organización de archivos"
    This reverts commit 8de1deafb3c2d597f9268e336a755580685ad2b3.

--- primerEntregaV2/vista/tp1ej3.php
+++ primerEntregaV2/vista/tp1ej3.php
@@ -2,67 +2,32 @@
     $titulo="EJERCICIO 3 TP1";
     include_once("estructura/cabecera.php");
 ?>  
-    <!-- <link rel="stylesheet" href="css/tp1ej3.css"> -->
-    <p class="h3">CONSIGNA:</p>
-<p class="lead">
-Crear una página php que contenga un formulario HTML como el que se indica en la imagen (darle formato con CSS), enviar estos datos por el método Post a otra página php que los reciba y muestre por pantalla un mensaje como el siguiente: “Hola, yo soy nombre , apellido tengo edad años y vivo en dirección”, usando la información recibida.
-    Cambiar el método Post por Get y analizar las diferencias.
-</p>
-    <!-- <form class="needs-validation" novalidate method="post" action="accion/tp1accion3.php" onSubmit="return validar2('dato')" > -->
-    <form class="needs-validation" novalidate method="post" action="accion/tp1accion3.php">
-    <div class="card card-primary col-md-6">
-        <!-- datos -->
-            <div class="card-body">
-                <!-- nombre -->
-                <div class="form-group">
-                    <label for="inputName">Nombre</label>
-                    <input type="text" class="form-control dato" name="nombre" id="nombre" pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+" required>
-                    <div class="invalid-feedback">
-                        Nombre debe comenzar con mayúscula y no poseer números.
-                    </div>
-                    <div class="valid-feedback">
-                        Nombre correcto.
-                    </div>
-                </div>
-                <!-- apellido -->
-                <div class="form-group">
-                    <label for="inputLastName">Apellido</label>
-                    <input type="text" class="form-control dato" name="apellido" id="apellido" pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+" required>
-                    <div class="invalid-feedback">
-                        Apellido debe comenzar con mayúscula y no poseer números.
-                    </div>
-                    <div class="valid-feedback">
-                        Apellido correcto.
-                    </div>
-                </div>
-                <!-- edad -->
-                <div class="form-group">
-                    <label for="inputAge">Edad</label>
-                    <input type="number" class="form-control dato" name="edad" id="edad" required>
-                    <div class="invalid-feedback">
-                        Valor de edad incorrecto.
-                    </div>
-                    <div class="valid-feedback">
-                        Edad correcta.
-                    </div>
-                </div>
-                <!-- direccion -->
-                <div class="form-group">
-                    <label for="inputAddres">Dirección</label>
-                    <input type="text" class="form-control dato" name="direccion" id="direccion" required>
-                    <div class="invalid-feedback">
-                        Valor de dirección incorrecto.
-                    </div>
-                    <div class="valid-feedback">
-                        Dirección correcta.
-                    </div>
-                </div>
-            </div>
-            <!-- submit -->
-            <div class="card-footer">
-                <button type="submit" name="submit" class="btn btn-primary float-right">Enviar</button>
-            </div>
-        </div>
+    <link rel="stylesheet" href="css/tp1ej3.css">
+    <h6>CONSIGNA: </h6>
+    <p>Crear una página php que contenga un formulario HTML como el que se indica en la imagen (darle formato con CSS), enviar estos datos por el método Post a otra página php que los reciba y muestre por pantalla un mensaje como el siguiente: “Hola, yo soy nombre , apellido tengo edad años y vivo en dirección”, usando la información recibida.
+    Cambiar el método Post por Get y analizar las diferencias</p>
+    <form method="post" action="accion/tp1accion3.php" onSubmit="return validar2('dato')" >
+        <table>
+            <tr>
+                <td>Nombre:</td>
+                <td><input type="text" name="nombre" class="dato"></td>
+            </tr>
+            <tr>
+                <td>Apellido:</td>
+                <td><input type="text" name="apellido" class="dato"></td>
+            </tr>
+            <tr>
+                <td>Edad:</td>
+                <td><input type="text" name="edad" class="dato"></td>
+            </tr>
+            <tr>
+                <td>Direccion:</td>
+                <td><input type="text" name="direccion" class="dato"></td>
+            </tr>
+            <tr>
+                <td><input type="submit"></td>
+            </tr>
+        </table>
     </form>
 <?php
     include_once("estructura/pie.php");
