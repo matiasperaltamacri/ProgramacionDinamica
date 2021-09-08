40ff1d5d27de2df66230e65b1ebbc8f9600d5ab5
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-08 17:25:04 -0300

    tp1ej4 con bootstrap y re-organización de archivos

--- primerEntregaV2/vista/accion/tp1accion4.php
+++ primerEntregaV2/vista/accion/tp1accion4.php
@@ -1,21 +1,20 @@
 <?php
     $titulo="EJERCICIO 4 TP1";
     include_once("../estructura/cabeceraAccion.php");
-    if($_GET){
-        $edad=$_GET['edad'];
-        $nombre=$_GET['nombre'];
-        $apellido=$_GET['apellido'];        
-        $direccion=$_GET['direccion'];
-        if ($edad<18){
-            
-            echo "Hola, yo soy $nombre $apellido tengo $edad años y vivo en $direccion.<br/>";
-        }
-        else{
-            echo "Hola, yo soy $nombre $apellido, soy mayor de edad y vivo en $direccion.<br/>";
-        }
-    }
-    else{
-        echo "No se recibieron datos.";
-    }
+    include_once "../../control/procesarTp1Ej4.php";
+include_once "../../utiles/funciones.php";
+
+$datos = data_submitted();
+$obj = new procesarTp1Ej4();
+$respuesta = $obj->procesarDatos($datos);
+
+if($respuesta != ""){
+    echo "<div class='alert alert-success' role='alert'><p>".$respuesta."</p></div>";
+}else{
+    echo "<div class='alert alert-danger' role='alert'>No se recibieron datos</div>";
+}
+echo "<a href='../tp1ej4.php'><button type='button' class='btn btn-info'>Regresar</button></a>";
+
+
     include_once("../estructura/pieAccion.php");
 ?>
