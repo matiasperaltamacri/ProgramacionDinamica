5610e64eedf8f135d9ac432654a2e5ebb2ceefc2
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-08 18:34:29 -0300

    implementación de la capa de control

--- primerEntregaV2/vista/accion/tp3accion1.php
+++ primerEntregaV2/vista/accion/tp3accion1.php
@@ -1,22 +1,42 @@
 <?php
-    $titulo="EJERCICIO 1 TP3";
-    include_once("../estructura/cabeceraAccion.php");
-    if($_FILES['miArchivo']["error"]<=0){
-        if ((($_FILES['miArchivo']['type']=="application/pdf")||($_FILES['miArchivo']['type']=="application/msword"))&&($_FILES["miArchivo"]["size"] < 2097152)){
-            $dir="../archivos/";
-            if(!copy($_FILES['miArchivo']['tmp_name'],$dir.$_FILES['miArchivo']['name'])){
-                echo "ERROR. No se pudo cargar el archivo";
-            }else{
-                $link=$dir.$_FILES['miArchivo']['name'];
-                echo "El archivo: ".$_FILES['miArchivo']['name']." se ha copiado con éxito.<br/>";
-                echo "<a href='$link'>Ubicacion del archivo</a>";
-            }
-        }else{
-            echo "ERROR. El archivo no fue guardado. Debe ser un pdf/doc no superior a 2mb.<br/>";
-        }
-        
-    }else{
-        echo "ERROR. No se pudo cargar/acceder el archivo.<br/>";
+$titulo = "EJERCICIO 1 TP3";
+include_once "../estructura/cabeceraAccion.php";
+include_once "../../control/procesarTp3Ej1.php";
+include_once "../../utiles/funciones.php";
+
+$datos = data_submitted();
+$obj = new procesarTp3Ej1();
+$respuesta = $obj->subirArchivo($datos);
+$colDir = $obj->obtenerArchivos();
+
+if ($respuesta == "") {
+    echo "<div class='alert alert-success' role='alert'>Archivo subido exitosamente.</div>";
+    $enlace = $obj->abrirArchivo($colDir);
+    if ($enlace != "") {
+        echo "<p><a href=" . $enlace . ">Archivo</a></p>";
     }
-    include_once("../estructura/pieAccion.php");
+
+} else {
+    echo "<div class='alert alert-danger' role='alert'>$respuesta</div>";
+}
+echo "<a href='../tp3ej1.php'><button type='button' class='btn btn-info'>Regresar</button></a>";
+
+// if($_FILES['miArchivo']["error"]<=0){
+//     if ((($_FILES['miArchivo']['type']=="application/pdf")||($_FILES['miArchivo']['type']=="application/msword"))&&($_FILES["miArchivo"]["size"] < 2097152)){
+//         $dir="../archivos/";
+//         if(!copy($_FILES['miArchivo']['tmp_name'],$dir.$_FILES['miArchivo']['name'])){
+//             echo "ERROR. No se pudo cargar el archivo";
+//         }else{
+//             $link=$dir.$_FILES['miArchivo']['name'];
+//             echo "El archivo: ".$_FILES['miArchivo']['name']." se ha copiado con éxito.<br/>";
+//             echo "<a href='$link'>Ubicacion del archivo</a>";
+//         }
+//     }else{
+//         echo "ERROR. El archivo no fue guardado. Debe ser un pdf/doc no superior a 2mb.<br/>";
+//     }
+
+// }else{
+//     echo "ERROR. No se pudo cargar/acceder el archivo.<br/>";
+// }
+include_once "../estructura/pieAccion.php";
 ?>
--- primerEntregaV2/vista/tp3ej1.php
+++ primerEntregaV2/vista/tp3ej1.php
@@ -2,12 +2,31 @@
 $titulo="EJERCICIO 1 TP3";
 include_once ("estructura/cabecera.php");
 ?>
-    <h6>CONSIGNA:</h6>
-    <p>Crear un formulario HTML que permita subir un archivo. En el servidor se deberá controlar, antes de guardar el archivo, que los tipos validos son <strong>.doc o pdf</strong> y además el tamaño máximo permitido es de <strong>2mb.</strong> En caso que se cumplan las condiciones mostrar un link al archivo cargado, en caso contrario mostrar un mensaje indicando el problema.</p>
+    <p class="h3">CONSIGNA:</p>
+<p class="lead">
+Crear un formulario HTML que permita subir un archivo. En el servidor se deberá controlar, antes de guardar el archivo, que los tipos validos son <strong>.doc o pdf</strong> y además el tamaño máximo permitido es de <strong>2mb.</strong> En caso que se cumplan las condiciones mostrar un link al archivo cargado, en caso contrario mostrar un mensaje indicando el problema.
+</p>
     <form method="post"  action="accion/tp3accion1.php" enctype="multipart/form-data">
-        <input type="file" id="miArchivo" name="miArchivo">
-        <br/>
-        <input type="submit">
+    <div class="row">
+            <div class="col-sm-4">
+            <div class="form-group has-feedback">
+                <label for="texto" class="control-label">Archivo Texto:</label>
+                <div class="input-group">
+                    <input type="file" class="form-control" id="texto" name ="texto" required>
+
+                </div>
+                <small class="text-muted">Archivo DOC/PDF - No superiores a 2 MB</small>
+                <span class="form-control-feedback" aria-hidden="true"></span>
+
+            </div>
+            </div>
+        </div>
+
+    <div class="row">
+            <div class="col-md-1 botones">
+                <input id="btn_eje1-sub" name="btn_eje1-sub" class="form-control btn-block btn-info" type="submit" value="Enviar">
+            </div>
+        </div>
     </form>
 <?php
     include_once("estructura/pie.php");
