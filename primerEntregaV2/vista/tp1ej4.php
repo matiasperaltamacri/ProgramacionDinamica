bc7184d0e39047b1bf45741f42af92a9649c69a5
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-08 17:25:04 -0300

    Revert "tp1ej4 con bootstrap y re-organización de archivos"
    This reverts commit 40ff1d5d27de2df66230e65b1ebbc8f9600d5ab5.

--- primerEntregaV2/vista/tp1ej4.php
+++ primerEntregaV2/vista/tp1ej4.php
@@ -2,68 +2,11 @@
     $titulo="EJERCICIO 4 TP1";
     include_once("estructura/cabecera.php");
 ?>
-<!-- <link rel="stylesheet" href="css/tp1ej3.css"> -->
-<p class="h3">CONSIGNA:</p>
-<p class="lead">
-Modificar el formulario del ejercicio anterior para que usando la edad solicitada, enviar esos datos a otra página en donde se muestren mensajes distintos dependiendo si la persona es mayor de edad o no; (si la edad es mayor o igual a 18).
-Enviar los datos usando el método GET y luego probar de modificar los datos directamente en la url para ver los dos posibles mensajes.
-</p>
-<form class="needs-validation" novalidate method="get" action="accion/tp1accion4.php">
-    <div class="card card-primary col-md-6">
-        <!-- datos -->
-            <div class="card-body">
-                <!-- nombre -->
-                <div class="form-group">
-                    <label for="inputName">Nombre</label>
-                    <input type="text" class="form-control dato" name="nombre" id="nombre" pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+" required>
-                    <div class="invalid-feedback">
-                        Nombre debe comenzar con mayúscula y no poseer números.
-                    </div>
-                    <div class="valid-feedback">
-                        Nombre correcto.
-                    </div>
-                </div>
-                <!-- apellido -->
-                <div class="form-group">
-                    <label for="inputLastName">Apellido</label>
-                    <input type="text" class="form-control dato" name="apellido" id="apellido" pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+" required>
-                    <div class="invalid-feedback">
-                        Apellido debe comenzar con mayúscula y no poseer números.
-                    </div>
-                    <div class="valid-feedback">
-                        Apellido correcto.
-                    </div>
-                </div>
-                <!-- edad -->
-                <div class="form-group">
-                    <label for="inputAge">Edad</label>
-                    <input type="number" class="form-control dato" name="edad" id="edad" required>
-                    <div class="invalid-feedback">
-                        Valor de edad incorrecto.
-                    </div>
-                    <div class="valid-feedback">
-                        Edad correcta.
-                    </div>
-                </div>
-                <!-- direccion -->
-                <div class="form-group">
-                    <label for="inputAddres">Dirección</label>
-                    <input type="text" class="form-control dato" name="direccion" id="direccion" required>
-                    <div class="invalid-feedback">
-                        Valor de dirección incorrecto.
-                    </div>
-                    <div class="valid-feedback">
-                        Dirección correcta.
-                    </div>
-                </div>
-            </div>
-            <!-- submit -->
-            <div class="card-footer">
-                <button type="submit" name="submit" class="btn btn-primary float-right">Enviar</button>
-            </div>
-        </div>
-    </form>
-    <!-- <form method="get" action="accion/tp1accion4.php" onSubmit="return validar2('dato')">
+<link rel="stylesheet" href="css/tp1ej3.css">
+<h6>CONSIGNA:</h6>
+<p>Modificar el formulario del ejercicio anterior para que usando la edad solicitada, enviar esos datos a otra página en donde se muestren mensajes distintos dependiendo si la persona es mayor de edad o no; (si la edad es mayor o igual a 18).
+Enviar los datos usando el método GET y luego probar de modificar los datos directamente en la url para ver los dos posibles mensajes.</p>
+    <form method="get" action="accion/tp1accion4.php" onSubmit="return validar2('dato')">
         <table>
             <tr>
                 <td>Nombre:</td>
@@ -85,7 +28,7 @@ Enviar los datos usando el método GET y luego probar de modificar los datos dir
                 <td><input type="submit"></td>
             </tr>
         </table>
-    </form> -->
+    </form>
 <?php
     include_once("estructura/pie.php");
 ?>
