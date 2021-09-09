fd1a3811390fcaab63bb79339e9877b5efdd62f6
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-09 01:39:08 -0300

    tp3ej2 implementando capa de control y comprobando contenido de txt

--- primerEntregaV2/vista/accion/tp3accion2.php
+++ primerEntregaV2/vista/accion/tp3accion2.php
@@ -1,21 +1,23 @@
 <?php
-    $titulo="EJERCICIO 2 TP3";
-    include_once("../estructura/cabeceraAccion.php");
-    if($_FILES['miArchivo']["error"]<=0){
-        if($_FILES['miArchivo']['type']=="text/plain"){
-            $dir='../archivos/';
-            if(copy($_FILES['miArchivo']['tmp_name'],$dir.$_FILES['miArchivo']['name'])){
-                $nombre=$_FILES['miArchivo']['name'];
-                $str=$dir.$nombre;
-                $contenido=file_get_contents($str);
-                echo "Contenido del archivo txt:<br/><textarea cols='70' rows='20'>$contenido</textarea>";
-            }
-                
-        }else{
-            echo "El archivo subido debe ser de tipo txt<br/>";
-        }
-    }else{
-        echo "ERROR. No se pudo cargar/acceder el archivo.<br/>";
-    }
-    include_once("../estructura/pieAccion.php");
+$titulo = "EJERCICIO 2 TP3";
+include_once "../estructura/cabeceraAccion.php";
+include_once "../../control/procesarTp3Ej2.php";
+include_once "../../utiles/funciones.php";
+
+$datos = data_submitted();
+$obj = new procesarTp3Ej2();
+$respuesta = $obj->subirArchivo($datos);
+$colDir = $obj->obtenerArchivos();
+
+if ($respuesta == "") {
+    echo "<div class='alert alert-success' role='alert'>Archivo subido exitosamente.</div>";
+    $content = $obj->abrirArchivo($colDir);
+    echo "<label for='Textarea'>Textarea con contenido de archivo</label>";
+    echo "<textarea class='form-control' rows='7'>" . $content . "</textarea>";
+} else {
+    echo "<div class='alert alert-danger' role='alert'>$respuesta</div>";
+}
+echo "<a href='../tp3ej2.php'><button type='button' class='btn btn-info'>Regresar</button></a>";
+
+include_once "../estructura/pieAccion.php";
 ?>
