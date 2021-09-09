d5859bc28b048bc8a40a754b91e0aa0d7642c7eb
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-09 03:42:34 -0300

    estilos modificados

--- primerEntregaV2/vista/accion/tp3accion3.php
+++ primerEntregaV2/vista/accion/tp3accion3.php
@@ -10,18 +10,25 @@ $resp = $obj->mostrar($datos);
 $arrayRespuesta = $obj->cargarImagen($datos);
 
 if ($arrayRespuesta["respCarga"] == "") {
-    $resp .= "<img src='" . $arrayRespuesta['enlace'] . "' style='width:300px'>";
+    $resp .= "<img class='rounded mx-auto d-block py-5' src='" . $arrayRespuesta['enlace'] . "' style='width:300px'>";
 } else {
     $resp .= "<div class='alert alert-danger' role='alert'>". $arrayRespuesta['respCarga'] . "</div>";
 }
 
 ?>
-<div class="card mt-5" style="width: 800px;background-color:#66CC99;margin:auto">
+<div class="card mt-5" style="width: 800px;background-color:#d1e7dd;margin:auto">
     <div class="card-body">
-        <h2 class="card-title" style="color:blue">La pelicula introducida es</h2>
+        <h2 class="card-title" style="color:#17a2b8">La pelicula introducida es</h2>
         <p class="card-text" style="color:#108800"><?php echo $resp ?></p>
     </div>
-    <a href='../tp3ej3.php'><button type='button' class='btn btn-info'>Regresar</button></a>
+    <div class="row">
+                <div class="col-9"></div>
+                <div class="col-3 mb-3 d-flex">
+                <a href='../tp3ej3.php'><button class="btn btn-primary text-white">Regresar</button></a>
+                </div>
+                
+            </div>
+    
 </div>
 <?php
 include_once "../estructura/pieAccion.php";
