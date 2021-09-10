a022e93e6f1643a438700a9094b965372331b8ff
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-10 13:46:29 -0300

    estilo de bootstrap aplicado a las páginas de acción

--- primerEntregaV2/vista/estructura/pieAccion.php
+++ primerEntregaV2/vista/estructura/pieAccion.php
@@ -3,8 +3,8 @@
 </div>
     <footer class="footer mt-auto py-3">
         <div class="container">
-            <h5>Programacion Dinamica - Primer Entrega</h5>
-            <strong>Grupo 2: Peralta-Romero-Gaspart</strong>
+            <h5 class="text-light">Programación Web Dinámica - Primer Entrega</h5>
+            <strong class="text-light">Grupo 2: Peralta-Romero-Gaspart</strong>
         </div>
     </footer>
     <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
